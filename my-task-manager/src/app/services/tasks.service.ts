696d4957ee8462d3173a499c45c1695ba6a5d0e1
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-04-06 16:04:23 -0500

    Implement task management features in TasksService: add task structure, retrieval, addition, and deletion methods using signals

--- my-task-manager/src/app/services/tasks.service.ts
+++ my-task-manager/src/app/services/tasks.service.ts
@@ -1,9 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
+
+export interface Task {
+  name: string;
+  description: string;
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TasksService {
+  private tasks = signal<Task[]>([
+    { name: 'Task 1', description: 'Description 1' },
+    { name: 'Task 2', description: 'Description 2' },
+  ]);
 
-  constructor() { }
+  getTasks() {
+    return this.tasks;
+  }
+  addTasks(newTask: Task) {
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+  deleteTask() {
+    this.tasks.update((tasks) => tasks.slice());
+  }
+  updateTask(updatedTask: Task) {
+    //TODO update task
+  }
 }
